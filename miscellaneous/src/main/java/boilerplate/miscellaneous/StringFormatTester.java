872c0ae8e017f831bd72ed82fd36abcddd1f7bc5
Willie <willie_chang@emphasis.com.tw>
2020-08-26 16:01:32 +0800

    add module miscellaneous

---
+++ miscellaneous/src/main/java/boilerplate/miscellaneous/StringFormatTester.java
@@ -0,0 +1,12 @@
+package boilerplate.miscellaneous;
+
+public class StringFormatTester {
+
+    private static String formatDouble() {
+        Double d = 0.1;
+        return String.format("%.2f", d);
+    }
+
+    public static void main(String[] args) {
+    }
+}
