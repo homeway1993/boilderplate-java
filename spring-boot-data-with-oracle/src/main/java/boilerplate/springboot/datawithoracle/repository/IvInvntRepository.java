4a2e56dd1fb8ee1ce6274f15597d9dbafa620efa
Willie <willie_chang@emphasis.com.tw>
2020-09-09 15:52:16 +0800

    fix for spring-boot-data-with-oracle

--- spring-boot-data-with-oracle/src/main/java/boilerplate/springboot/datawithoracle/repository/IvInvntRepository.java
+++ spring-boot-data-with-oracle/src/main/java/boilerplate/springboot/datawithoracle/repository/IvInvntRepository.java
@@ -2,6 +2,16 @@ package boilerplate.springboot.datawithoracle.repository;
 
 import boilerplate.springboot.datawithoracle.entity.IvInvnt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
 public interface IvInvntRepository extends JpaRepository<IvInvnt, String> {
+
+    @Modifying
+    @Query(value = "update sys_poss.IV_INVNT set INVNT_STAT_CDE = ?1 where INVNT_ID in (?2)", nativeQuery = true)
+    void updateStatusCodeByInventoryIds(String statusCode, List<String> inventoryIds);
 }
