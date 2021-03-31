24a8f38ef7a5b7ff423b73850923c72899ece51c
Willie <willie_chang@emphasis.com.tw>
2021-03-31 14:48:52 +0800

    add rabbitmq-web-stomp module

---
+++ rabbitmq-web-stomp/src/main/java/boilerplate/rabbitmq/webstomp/WebStompApplication.java
@@ -0,0 +1,12 @@
+package boilerplate.rabbitmq.webstomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStompApplication.class, args);
+    }
+}
